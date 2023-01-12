a7a1fba87bfe0140eb1ebcc6b6d3758ab3e0b0af
soonable@naver.com <soonable@naver.com>
2023-01-12 23:50:56 +0900

    #2 Test : Add UserRepositoryTest and Test complete

--- src/main/java/com/soonhankwon/coffeeplzbackend/repository/UserRepository.java
+++ src/main/java/com/soonhankwon/coffeeplzbackend/repository/UserRepository.java
@@ -1,2 +1,11 @@
-package com.soonhankwon.coffeeplzbackend.repository;public interface UserRepository {
+package com.soonhankwon.coffeeplzbackend.repository;
+
+import com.soonhankwon.coffeeplzbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByLoginId(String loginId);
+    List<User> findAll();
 }
--- src/test/java/com/soonhankwon/coffeeplzbackend/repository/UserRepositoryTest.java
+++ src/test/java/com/soonhankwon/coffeeplzbackend/repository/UserRepositoryTest.java
@@ -1,12 +1,8 @@
-package com.soonhankwon.coffeeplzbackend;
+package com.soonhankwon.coffeeplzbackend.repository;
 
-import com.soonhankwon.coffeeplzbackend.dto.SignupRequestDto;
 import com.soonhankwon.coffeeplzbackend.entity.User;
-import com.soonhankwon.coffeeplzbackend.repository.UserRepository;
-import com.soonhankwon.coffeeplzbackend.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -19,63 +15,56 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceTest {
+class UserRepositoryTest {
     @Mock
     UserRepository userRepository;
 
-    @InjectMocks
-    UserService userService;
-
     @Test
-    public void saveNewUser() {
-        //given
-        SignupRequestDto signupRequestDto = SignupRequestDto.builder()
-                .loginId("soonhan")
-                .email("soonable@gmail.com")
-                .password("1234")
-                .point(0L).build();
-
-        User user = userRepository.findByLoginId(signupRequestDto.getLoginId());
-        when(userRepository.save(any())).thenReturn(user);
-
-        //when
-        String result = userService.signupUser(SignupRequestDto.builder().loginId("soonhan").build());
-        //then
-        verify(userRepository, times(1)).save(any());
-        assertThat(result, equalTo("success"));
-    }
-
-    @Test void findUser() {
+    void saveTest() {
         //given
         User user = User.builder().id(1L)
                 .loginId("soonhan")
                 .email("soonable@gmail.com")
                 .password("1234")
                 .point(0L).build();
-        when(userRepository.findByLoginId("soonhan")).thenReturn(user);
 
+        when(userRepository.save(any())).thenReturn(user);
         //when
-        User result = userService.findUser("soonhan");
+        User result = userRepository.save(User.builder().loginId("soonhan").build());
+        //then
+        verify(userRepository, times(1)).save(any());
+        assertThat(result, equalTo(user));
+    }
+    @Test
+    void findByLoginId() {
+        //given
+        User user = User.builder().id(1L)
+                .loginId("soonhan")
+                .email("soonable@gmail.com")
+                .password("1234")
+                .point(0L).build();
 
+        when(userRepository.findByLoginId(any())).thenReturn(user);
+        //when
+        User result = userRepository.findByLoginId("soonhan");
         //then
         verify(userRepository, times(1)).findByLoginId(any());
-        assertThat(result, equalTo(user));
+        assertThat(result.getLoginId(), equalTo(user.getLoginId()));
     }
 
     @Test
-    public void findAllUsers() {
+    void findAll() {
         //given
         List<User> users =
                 Arrays.asList(User.builder().id(1L).loginId("soonhan").build(),
                         User.builder().id(2L).loginId("kyuri").build());
 
         when(userRepository.findAll()).thenReturn(users);
-
         //when
-        List<User> result = userService.findAllUsers();
-
+        List<User> result = userRepository.findAll();
         //then
         verify(userRepository, times(1)).findAll();
-        assertThat(result,equalTo(users));
+        assertThat(result.get(0).getLoginId(), equalTo(users.get(0).getLoginId()));
+        assertThat(result.get(1).getLoginId(), equalTo(users.get(1).getLoginId()));
     }
 }

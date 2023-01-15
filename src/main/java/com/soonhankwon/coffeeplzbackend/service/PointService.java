95d2e66f3626b02e91ddca81d1686447ac225542
soonable@naver.com <soonable@naver.com>
2023-01-15 11:47:29 +0900

    fix : dirty checking 으로 인해 수정시 save 불필요

--- src/main/java/com/soonhankwon/coffeeplzbackend/service/PointService.java
+++ src/main/java/com/soonhankwon/coffeeplzbackend/service/PointService.java
@@ -1,9 +1,7 @@
 package com.soonhankwon.coffeeplzbackend.service;
 
 import com.soonhankwon.coffeeplzbackend.dto.response.PointResponseDto;
-import com.soonhankwon.coffeeplzbackend.entity.PointHistory;
 import com.soonhankwon.coffeeplzbackend.entity.User;
-import com.soonhankwon.coffeeplzbackend.repository.PointRepository;
 import com.soonhankwon.coffeeplzbackend.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +14,6 @@ public class PointService {
     public PointResponseDto chargePoint(String loginId, Long chargePoint) {
         User user = userRepository.findByLoginId(loginId);
         user.setUserPoint(user.getPoint() + chargePoint);
-        userRepository.save(user);
         return new PointResponseDto("포인트 충전 완료");
     }
 }

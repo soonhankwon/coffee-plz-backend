f752cea74eb94ebbab25a69c45eef196f1cb7b4c
soonable@naver.com <soonable@naver.com>
2023-01-17 00:41:10 +0900

    feat : DDL 작성에 따른 Entity 변경 및 Mapping

---
+++ src/main/java/com/soonhankwon/coffeeplzbackend/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.soonhankwon.coffeeplzbackend.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+    @Id
+    @Column(name = "oreder_item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "item_id", nullable = false)
+    private Long itemId;
+
+    @Column(name = "item_category", nullable = false)
+    private String itemCategory;
+
+    @Column(name = "item_name", nullable = false)
+    private String itemName;
+
+    @Column(name = "item_price", nullable = false)
+    private Long itemPrice;
+
+    @Column(name = "item_size", nullable = false)
+    private Long item_size;
+
+    @Column(name = "item_quantity", nullable = false)
+    private Integer item_quantity;
+}

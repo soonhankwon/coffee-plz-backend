edcd29ff1d42bde2ab89fc564fdbe7b79d0feb8d
soonable@naver.com <soonable@naver.com>
2023-01-19 00:02:45 +0900

    test : Add 메뉴 변경 & 삭제 테스트 코드

--- src/test/java/com/soonhankwon/coffeeplzbackend/service/ItemServiceTest.java
+++ src/test/java/com/soonhankwon/coffeeplzbackend/service/ItemServiceTest.java
@@ -1,9 +1,11 @@
 package com.soonhankwon.coffeeplzbackend.service;
 
 import com.soonhankwon.coffeeplzbackend.dto.request.ItemRequestDto;
+import com.soonhankwon.coffeeplzbackend.dto.response.GlobalResponseDto;
 import com.soonhankwon.coffeeplzbackend.dto.response.ItemResponseDto;
 import com.soonhankwon.coffeeplzbackend.entity.Item;
 import com.soonhankwon.coffeeplzbackend.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -62,7 +64,7 @@ class ItemServiceTest {
     @Test
     void addItem() {
         //given
-        ItemRequestDto itemRequestDto = new ItemRequestDto("Americano","Small", 2500L);
+        ItemRequestDto itemRequestDto = new ItemRequestDto("Americano", "Small", 2500L);
         Item item = Item.builder().id(1L)
                 .name(itemRequestDto.getName())
                 .size(itemRequestDto.getSize())
@@ -78,5 +80,46 @@ class ItemServiceTest {
         assertThat(result.getName(), equalTo("Americano"));
         assertThat(result.getSize(), equalTo("Small"));
         assertThat(result.getPrice(), equalTo(2500L));
+    }
+
+    @Test
+    @DisplayName("메뉴 업데이트 테스트")
+    void updateItem() {
+        //given
+        Long id = 1L;
+        String name = "Espresso";
+        String name2 = "Americano";
+        String size = "Small";
+        Long price = 3000L;
+        ItemRequestDto itemRequestDto = new ItemRequestDto(name, size, price);
+        Item item = new Item(id, name2, size, price);
+        when(itemRepository.findById(id)).thenReturn(Optional.of(item));
+
+        //when
+        ItemResponseDto result = itemService.updateItem(id, itemRequestDto);
+
+        //then
+        assertThat(result.getName(), equalTo("Espresso"));
+        assertThat(result.getSize(), equalTo("Small"));
+        assertThat(result.getPrice(), equalTo(3000L));
+
+    }
+
+    @Test
+    @DisplayName("메뉴 삭제 테스트")
+    void deleteItem() {
+        //given
+        Long id = 1L;
+        String name = "Americano";
+        String size = "Small";
+        Long price = 3000L;
+        Item item = new Item(id, name, size, price);
+        when(itemRepository.findById(id)).thenReturn(Optional.of(item));
+
+        //when
+        GlobalResponseDto result = itemService.deleteItem(id);
+
+        //then
+        assertThat(result.getMessage(), equalTo("삭제 완료"));
     }
 }

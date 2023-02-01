6256b12da83adecec6a4208aafec8ce84ddcd38e
soonable@naver.com <soonable@naver.com>
2023-02-02 01:14:17 +0900

    feat : 주문 시 redisson distributed lock 적용 & 멀티 쓰레드 테스트 완료 (#11)

--- src/main/java/com/soonhankwon/coffeeplzbackend/service/OrderService.java
+++ src/main/java/com/soonhankwon/coffeeplzbackend/service/OrderService.java
@@ -13,10 +13,13 @@ import com.soonhankwon.coffeeplzbackend.repository.OrderRepository;
 import com.soonhankwon.coffeeplzbackend.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -27,11 +30,31 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final UserRepository userRepository;
     private final DataCollectionService dataCollectionService;
+    private final RedissonClient redissonClient;
 
     public List<OrderResponseDto> findAllOrders() {
         return orderRepository.findAll().stream().map(OrderResponseDto::new).collect(Collectors.toList());
     }
+    public OrderResponseDto orderProcessingWithLock(Long userId, List<OrderRequestDto> orderRequestDto) {
+        String lockName = "orderProcessLock";
+        RLock lock = redissonClient.getLock(lockName);
+        String worker = Thread.currentThread().getName();
+        OrderResponseDto orderResponseDto;
 
+        try {
+            boolean available = lock.tryLock(0, 2, TimeUnit.SECONDS);
+            if (!available) {
+                throw new RuntimeException("Lock 을 획득하지 못했습니다.");
+            }
+            log.info("현재 {}서버에서 작업중입니다.", worker);
+            orderResponseDto = orderProcessing(userId, orderRequestDto);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+        return orderResponseDto;
+    }
     public OrderResponseDto orderProcessing(Long userId, List<OrderRequestDto> orderRequestDto) {
         User user = userRepository.findById(userId).orElseThrow(NullPointerException::new);
         boolean previousOrderExists = orderRepository.existsByUserIdAndStatus(userId, Order.OrderStatus.ORDERED);

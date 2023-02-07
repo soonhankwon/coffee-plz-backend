6a9a8bf20c2d06f5c02e05f16e0b3bcc6df901b7
soonable@naver.com <soonable@naver.com>
2023-02-07 16:27:43 +0900

    fix : add operation -> api 기능 설명 추가 (#3)

--- src/main/java/com/soonhankwon/coffeeplzbackend/controller/ItemController.java
+++ src/main/java/com/soonhankwon/coffeeplzbackend/controller/ItemController.java
@@ -4,6 +4,7 @@ import com.soonhankwon.coffeeplzbackend.dto.request.ItemRequestDto;
 import com.soonhankwon.coffeeplzbackend.dto.response.GlobalResponseDto;
 import com.soonhankwon.coffeeplzbackend.dto.response.ItemResponseDto;
 import com.soonhankwon.coffeeplzbackend.service.ItemService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,33 +18,40 @@ import java.util.List;
 public class ItemController {
     private final ItemService itemService;
     @GetMapping("/item")
+    @Operation(summary = "전체 메뉴 조회")
     public ResponseEntity<List<ItemResponseDto>> findAllItem() {
         return ResponseEntity.status(200).body(itemService.findAllItem());
     }
     @GetMapping("/item/{itemId}")
+    @Operation(summary = "단일 메뉴 조회")
     public ResponseEntity<ItemResponseDto> findItem(@PathVariable Long itemId) {
         return ResponseEntity.status(200).body(itemService.findItem(itemId));
     }
     @GetMapping("/item/favorite")
+    @Operation(summary = "7일간 인기메뉴 조회")
     public ResponseEntity<List<ItemResponseDto>> favoriteItems() {
         return ResponseEntity.status(200).body(itemService.favoriteItems());
     }
     @GetMapping("/item/favorite/test")
+    @Operation(summary = "인기메뉴 조회 테스트")
     public void favoriteItemsTest() {
         itemService.updateFavoriteItems();
     }
 
     @PostMapping("/item/add")
+    @Operation(summary = "메뉴 추가")
     public ResponseEntity<ItemResponseDto> addItem(ItemRequestDto itemRequestDto) {
         return ResponseEntity.status(200).body(itemService.addItem(itemRequestDto));
     }
 
     @PatchMapping("/item/{itemId}")
+    @Operation(summary = "메뉴 업데이트")
     public ResponseEntity<ItemResponseDto> updateItem(@PathVariable Long itemId, ItemRequestDto itemRequestDto) {
         return ResponseEntity.status(200).body(itemService.updateItem(itemId, itemRequestDto));
     }
 
     @DeleteMapping("/item/delete/{itemId}")
+    @Operation(summary = "메뉴 삭제")
     public ResponseEntity<GlobalResponseDto> deleteItem(@PathVariable Long itemId) {
         return ResponseEntity.status(200).body(itemService.deleteItem(itemId));
     }

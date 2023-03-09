879942c9a564c68cc588edcd7a7e5b90cd1a4f20
soonable@naver.com <soonable@naver.com>
2023-03-10 01:27:29 +0900

    feat : Dto Abstract Factory

--- src/main/java/com/soonhankwon/coffeeplzbackend/factory/DataCollectionDtoFactory.java
+++ src/main/java/com/soonhankwon/coffeeplzbackend/factory/DataCollectionDtoFactory.java
@@ -5,7 +5,7 @@ import com.soonhankwon.coffeeplzbackend.dto.OrderDataCollectionDto;
 import java.util.List;
 
 public abstract class DataCollectionDtoFactory {
-     public static OrderDataCollectionDto createOrderDataCollectionDto(Long userId, List<Long> itemIds, Long totalPrice) {
+    public static OrderDataCollectionDto createOrderDataCollectionDto(Long userId, List<Long> itemIds, Long totalPrice) {
         return OrderDataCollectionDto.createOrderDataCollectionDto(userId, itemIds, totalPrice);
     }
 }

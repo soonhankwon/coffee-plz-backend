eefb6717476b18ff67c7171be9b808fec036c4dc
soonable@naver.com <soonable@naver.com>
2023-04-20 00:06:04 +0900

    refactor : 매개변수 객체로

--- src/main/java/com/soonhankwon/coffeeplzbackend/service/OrderService.java
+++ src/main/java/com/soonhankwon/coffeeplzbackend/service/OrderService.java
@@ -1,12 +1,12 @@
 package com.soonhankwon.coffeeplzbackend.service;
 
-import com.soonhankwon.coffeeplzbackend.dto.request.OrderRequestDto;
+import com.soonhankwon.coffeeplzbackend.dto.OrderDto;
 import com.soonhankwon.coffeeplzbackend.dto.response.OrderResponseDto;
 
 import java.util.List;
 
 public interface OrderService {
     List<OrderResponseDto> findAllOrders();
-    OrderResponseDto placeOrder(Long userId, List<OrderRequestDto> orderRequestDto);
-    OrderResponseDto orderProcessing(Long userId, List<OrderRequestDto> orderRequestDto);
+    OrderResponseDto placeOrder(OrderDto orderDto);
+    OrderResponseDto orderProcessing(OrderDto orderDto);
 }

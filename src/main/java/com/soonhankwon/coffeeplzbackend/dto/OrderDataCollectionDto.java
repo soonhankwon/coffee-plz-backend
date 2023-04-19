cfc182b09075b98d40ddc7e57c6e54eeac2640dc
soonable@naver.com <soonable@naver.com>
2023-04-20 00:08:30 +0900

    refactor : 정적 메서드 제거 & 생성자로 대체

--- src/main/java/com/soonhankwon/coffeeplzbackend/dto/OrderDataCollectionDto.java
+++ src/main/java/com/soonhankwon/coffeeplzbackend/dto/OrderDataCollectionDto.java
@@ -6,12 +6,9 @@ public class OrderDataCollectionDto {
     private Long userId;
     private List<Long> itemIds;
     private Long totalPrice;
-    private OrderDataCollectionDto(Long userId, List<Long> itemIds, Long totalPrice) {
+    public OrderDataCollectionDto(Long userId, List<Long> itemIds, Long totalPrice) {
         this.userId = userId;
         this.itemIds = itemIds;
         this.totalPrice = totalPrice;
-    }
-    public static OrderDataCollectionDto createOrderDataCollectionDto(Long userId, List<Long> itemIds, Long totalPrice) {
-        return new OrderDataCollectionDto(userId, itemIds, totalPrice);
     }
 }

4e1ab3e22a034c871b4707925bc34c0839ab0fa4
soonable@naver.com <soonable@naver.com>
2023-04-20 00:02:45 +0900

    feat : OrderServiceSystem 인터페이스 구현 클래스

---
+++ src/main/java/com/soonhankwon/coffeeplzbackend/service/OrderServiceSystemImpl.java
@@ -0,0 +1,35 @@
+package com.soonhankwon.coffeeplzbackend.service;
+
+import com.soonhankwon.coffeeplzbackend.dto.OrderDto;
+import com.soonhankwon.coffeeplzbackend.dto.response.OrderResponseDto;
+import com.soonhankwon.coffeeplzbackend.dto.response.OrderSheetResDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderServiceSystemImpl implements OrderServiceSystem {
+    private final OrderService orderServiceImpl;
+    private final OrderSheetService orderSheetService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<OrderResponseDto> findAllOrders() {
+        return orderServiceImpl.findAllOrders();
+    }
+    @Override
+    public OrderResponseDto placeOrder(OrderDto orderDto) {
+        return orderServiceImpl.placeOrder(orderDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public OrderSheetResDto findOrderSheet(Long userId) {
+        return orderSheetService.findOrderSheet(userId);
+    }
+}

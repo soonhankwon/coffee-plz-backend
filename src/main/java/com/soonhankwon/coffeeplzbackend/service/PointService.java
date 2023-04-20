779a58d3f4580e2e926f3eaca28417dacda7fbe7
soonable@naver.com <soonable@naver.com>
2023-04-20 20:34:07 +0900

    refactor : static 메서드 제거 & 생성자로 객체 저장

--- src/main/java/com/soonhankwon/coffeeplzbackend/service/PointService.java
+++ src/main/java/com/soonhankwon/coffeeplzbackend/service/PointService.java
@@ -1,17 +1,16 @@
 package com.soonhankwon.coffeeplzbackend.service;
 
-import com.soonhankwon.coffeeplzbackend.dto.response.PointResponseDto;
-import com.soonhankwon.coffeeplzbackend.domain.PointHistory;
-import com.soonhankwon.coffeeplzbackend.domain.User;
 import com.soonhankwon.coffeeplzbackend.common.exception.ErrorCode;
 import com.soonhankwon.coffeeplzbackend.common.exception.RequestException;
+import com.soonhankwon.coffeeplzbackend.domain.PointHistory;
+import com.soonhankwon.coffeeplzbackend.domain.PointType;
+import com.soonhankwon.coffeeplzbackend.domain.User;
+import com.soonhankwon.coffeeplzbackend.dto.response.PointResponseDto;
 import com.soonhankwon.coffeeplzbackend.repository.PointHistoryRepository;
 import com.soonhankwon.coffeeplzbackend.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import static com.soonhankwon.coffeeplzbackend.domain.PointHistory.createPointHistory;
 
 @AllArgsConstructor
 @Service
@@ -23,8 +22,8 @@ public class PointService {
     public PointResponseDto chargePoint(Long id, Long chargePoint) {
         User user = userRepository.findById(id).orElseThrow(
                 () -> new RequestException(ErrorCode.USER_NOT_FOUND));
-        pointHistoryRepository.save(createPointHistory(user, PointHistory.PointType.CHARGE, chargePoint));
-        user.setUserPointWithValidChargePoint(chargePoint);
+        pointHistoryRepository.save(new PointHistory(user, PointType.CHARGE, chargePoint));
+        user.setUserPointValidChargePoint(chargePoint);
         return new PointResponseDto("포인트 충전 완료");
     }
 }
